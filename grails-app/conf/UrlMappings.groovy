d4c865fdd6e838e27503c6170463d6269fb63a81
Piotr Dorobisz <pdorobisz@gmail.com>
2013-02-20 01:58:24 +0100

    cleanup

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,6 @@
 class UrlMappings {
 
 	static mappings = {
-//		"/$controller/$action?/$id?"{
-//			constraints {
-//				// apply constraints here
-//			}
-//		}
-//
-//		"/"(view:"/index")
 		"500"(view:'/error')
         "/download/$root/$path**"(controller: "download", action: "index")
 	}
